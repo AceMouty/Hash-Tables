f67c50b6bd78356b76c45dd00f6cb63abe7ad6ae
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-11 16:29:35 -0600

    Completed MVP

--- src/hashtable.py
+++ src/hashtable.py
@@ -10,7 +10,7 @@ class LinkedPair:
         self.next = None
 
 
-class HashTable:
+class HashTableArray:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
@@ -68,16 +68,18 @@ class HashTable:
         Fill this in.
         '''
         index = self._hash_mod(key)
+        old_item = None
 
         if self.storage[index] is not None:
             if self.storage[index][0] == key:
+                old_item = self.storage[index]
                 self.storage[index] = None
             else:
                 print(f"WARNING: Collision has occured at {index}")
         else:
             print(f"WARNING: key {key} not found")
 
-        return
+        return old_item
 
     def retrieve(self, key):
         '''
@@ -107,11 +109,136 @@ class HashTable:
         Fill this in.
         '''
         old_storage = self.storage
+        print(old_storage)
         self.capacity *= 2
         self.storage = [None] * self.capacity  # None 16x in an array
 
         for item in old_storage:
-            self.insert(item[0], item[1])
+            if item is not None:
+                self.insert(item[0], item[1])
+
+class HashTable:
+    '''
+    A hash table that with `capacity` buckets
+    that accepts string keys
+    '''
+    def __init__(self, capacity):
+        self.capacity = capacity  # Number of buckets in the hash table
+        self.storage = [None] * capacity
+
+    def _hash(self, key):
+        '''
+        Hash an arbitrary key and return an integer.
+
+        You may replace the Python hash with DJB2 as a stretch goal.
+        '''
+        return hash(key)
+
+    def _hash_djb2(self, key):
+        '''
+        Hash an arbitrary key using DJB2 hash
+
+        OPTIONAL STRETCH: Research and implement DJB2
+        '''
+        pass
+
+    def _hash_mod(self, key):
+        '''
+        Take an arbitrary key and return a valid integer index
+        within the storage capacity of the hash table.
+        '''
+        return self._hash(key) % self.capacity
+
+    def insert(self, key, value):
+        '''
+        Store the value with the given key.
+
+        Hash collisions should be handled with Linked List Chaining.
+
+        Fill this in.
+        '''
+        index = self._hash_mod(key)
+
+        # add to the head of the list if index is not None
+        if self.storage[index] is not None:
+            # create a new node
+            new_node = LinkedPair(key, value)
+            # refrence the next node of the new node to be the current head
+            new_node.next = self.storage[index]
+            # set the array index to be the newly created node
+            self.storage[index] = new_node
+            print(f"WARNING: Collision has occured, at {index}, creating a new node")
+            return
+        else:
+            self.storage[index] = LinkedPair(key, value)
+            return
+
+    def remove(self, key):
+        '''
+        Remove the value stored with the given key.
+
+        Print a warning if the key is not found.
+
+        Fill this in.
+        '''
+        index = self._hash_mod(key)
+        current_node = self.storage[index]
+        prev_node = None
+
+        # IF we have a match at the head of the LL
+        if current_node.key == key:
+            self.storage[index] = current_node.next
+
+        if current_node.key != key:  # traverse the linked list
+            while current_node.next is not None:
+                # current node before we move
+                prev_node = current_node  # line_1, 7
+                # move forward
+                current_node = current_node.next
+                if current_node.key == key:
+                    prev_node.next = current_node.next
+                    return None
+            print(f"WARNING: Key {key} not found")
+
+    def retrieve(self, key):
+        '''
+        Retrieve the value stored with the given key.
+
+        Returns None if the key is not found.
+
+        Fill this in.
+        '''
+        index = self._hash_mod(key)
+        current_node = self.storage[index]
+
+        if current_node is None:
+            return None
+        elif current_node.key == key:
+            return current_node.value
+
+        if current_node.key != key:
+            while current_node.next is not None:
+                current_node = current_node.next
+                if current_node.key == key:
+                    return current_node.value
+            return None
+
+    def resize(self):
+        '''
+        Doubles the capacity of the hash table and
+        rehash all key/value pairs.
+
+        Fill this in.
+        '''
+        old_storage = self.storage
+        self.capacity *= 2
+        self.storage = [None] * self.capacity  # None 16x in an array
+        current_item = None
+        for item in old_storage:
+            current_item = item
+            while current_item is not None:
+                self.insert(current_item.key, current_item.value)
+                current_item = current_item.next
 
 
 if __name__ == "__main__":
@@ -119,14 +246,14 @@ if __name__ == "__main__":
 
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
-    # ht.insert("line_3", "Linked list saves the day!")
+    ht.insert("line_3", "Linked list saves the day!")
 
     print("")
 
     # Test storing beyond capacity
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
-    # print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_3"))
 
     # Test resizing
     old_capacity = len(ht.storage)
@@ -135,9 +262,9 @@ if __name__ == "__main__":
 
     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
+    print("Test if data intact after resizing")
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
-    # print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_3"))
 
-    print("")
+    # print("")
